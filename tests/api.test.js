cefa2968dcb15f5cf79dff9428bf634f8ec5a488
boujee <>
2018-05-11 14:45:30 -0700

    update

--- tests/api.test.js
+++ tests/api.test.js
@@ -12,20 +12,20 @@ const mkAPI = () => api({API_KEY});
 const tap = f => a => { f(a); return a; };
 const getBody = (uri) => request(mkAPI()).get(uri).expect(200).then(({body}) => body);
 const getJson = (uri) => getBody(uri).then(tap(body => expect(body).not.toBeInstanceOf(String)));
-const toHaveProperties = (...arr) => e => arr.forEach(expect(e).toHaveProperty(e));
+const toHaveProperties = (...arr) => e => arr.forEach(p => expect(e).toHaveProperty(p));
 
 describe('api', () => {
   test('I can get the image URLs, alt text and page urls for a set of images relating to a given search string.', () =>
     getJson('/api/imagesearch/lol cats')
       .then(tap(body => expect(body).toBeInstanceOf(Array)))
-      .then(tap(body => expect(body).toHaveProperty(0)))
+      .then(tap(body => expect(body).toHaveProperty('0')))
       .then(body => body[0])
       .then(toHaveProperties('url', 'snippet', 'thumbnail', 'context'))
   );
   test('I can paginate through the responses by adding a ?offset=2 parameter to the URL', () =>
     Promise.all([getJson('/api/imagesearch/lol cats'), getJson('/api/imagesearch/lol cats?offset=2')])
       .then(([a, b]) => [a.slice(2).concat(b.slice(-2)), b])
-      .then(([a, b]) => expect(a).toBe(b))
+      .then(([a, b]) => expect(a).toEqual(b))
   );
   test('I can get a list of the most recently submitted search strings', () =>
     getJson('/api/imagesearch/lol cats b')
@@ -35,6 +35,6 @@ describe('api', () => {
       .then(tap(body => body.forEach(toHaveProperties('term', 'when'))))
       .then(body => body.map(r => r.term))
       .then(body => body.slice(0, 2))
-      .then(tap(body => expect(body).toBe(['lol cats a', 'lol cats b'])))
+      .then(tap(body => expect(body).toEqual(['lol cats a', 'lol cats b'])))
   );
 });
