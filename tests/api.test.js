96e7f29c604aa687b3bb4ca970b9509fa64cdb5c
boujee <>
2018-05-12 14:22:59 -0700

    refactor tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -19,30 +19,28 @@ const getBody = uri => request(app).get(uri).expect(200).then(({body}) => body);
 
 const getJson = uri => getBody(uri).then(tap(body => expect(body).not.toBeInstanceOf(String)));
 
-const toHaveProperties = (...arr) => e => arr.forEach(p => expect(e).toHaveProperty(p));
+const toHaveProperties = (...props) => e => props.forEach(p => e.toHaveProperty(p));
 
-describe('api', () => {
-  test('I can get the image URLs, alt text and page urls for a set of images relating to a given search string.', () =>
-    getJson('/api/imagesearch/lol cats')
-      .then(tap(body => expect(body).toBeInstanceOf(Array)))
-      .then(tap(body => expect(body).toHaveProperty('0')))
-      .then(body => body[0])
-      .then(toHaveProperties('url', 'snippet', 'thumbnail', 'context'))
-  );
-  test('I can paginate through the responses by adding a ?offset=2 parameter to the URL', () =>
-    Promise.all([getJson('/api/imagesearch/lol cats'), getJson('/api/imagesearch/lol cats?offset=2')])
-      .then(([a, b]) => [a.slice(2).concat(b.slice(-2)), b])
-      .then(([a, b]) => expect(a).toEqual(b))
-  );
-  test('I can get a list of the most recently submitted search strings', () =>
-    getJson('/api/imagesearch/lol cats b')
-      .then(() => getJson('/api/imagesearch/lol cats a'))
-      .then(() => getJson('/api/latest/imagesearch'))
-      .then(tap(body => expect(body).toBeInstanceOf(Array)))
-      .then(tap(body => expect(body.length).toBeGreaterThanOrEqual(2)))
-      .then(tap(body => body.forEach(toHaveProperties('term', 'when'))))
-      .then(body => body.map(r => r.term))
-      .then(body => body.slice(0, 2))
-      .then(tap(body => expect(body).toEqual(['lol cats a', 'lol cats b'])))
-  );
+test('I can get the image URLs, alt text and page urls for a set of images relating to a given search string.', async () => {
+  const json = await getJson('/api/imagesearch/lol cats');
+  expect(json).toBeInstanceOf(Array);
+  expect(json.length).toBeGreaterThan(0);
+  json.map(_ => expect(_)).forEach(toHaveProperties('url', 'snippet', 'thumbnail', 'context'));
+});
+
+test('I can paginate through the responses by adding a ?offset=2 parameter to the URL', async () => {
+  const a = await getJson('/api/imagesearch/lol cats');
+  const b = await getJson('/api/imagesearch/lol cats?offset=2');
+  const c = a.slice(2).concat(b.slice(-2));
+  expect(c).toEqual(b);
+});
+
+test('I can get a list of the most recently submitted search strings', async () => {
+  await getJson('/api/imagesearch/lol cats b');
+  await getJson('/api/imagesearch/lol cats a');
+  const json = await getJson('/api/latest/imagesearch');
+  expect(json).toBeInstanceOf(Array);
+  expect(json.length).toBeGreaterThanOrEqual(2);
+  json.map(_ => expect(_)).forEach(toHaveProperties('term', 'when'));
+  expect(json.map(r => r.term).slice(0, 2)).toEqual(['lol cats a', 'lol cats b']);
 });
